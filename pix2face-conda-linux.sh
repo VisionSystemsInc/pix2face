8f9fd231f053c9a964ba1f9379e1f8b6235bcb26
Daniel Crispell <dan@visionsystemsinc.com>
2017-06-14 11:15:59 -0400

    extract minimum required from STR's setup script

--- pix2face-conda-linux.sh
+++ pix2face-conda-linux.sh
@@ -1,10 +1,17 @@
 # Source this file to set up pix2face environment.
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # add anaconda to path
-export PATH="$DIR"/../anaconda2/bin:$PATH
-export JANUS_ROOT="$DIR"/janus_root
+export JANUS_CONDA_ROOT="$DIR"/../anaconda2
+export PATH="$JANUS_CONDA_ROOT"/bin:$PATH
+#export JANUS_ROOT="$DIR"/janus_root
+export JANUS_PLATFORM=centos7
+export JANUS_RELEASE=${JANUS_ROOT}/${JANUS_PLATFORM}  # symlinked to nfs mount
+export NVIDIA_LIB_DIR=/usr/lib64/nvidia
+export LD_LIBRARY_PATH=$JANUS_CONDA_ROOT/lib:$NVIDIA_LIB_DIR:$LD_LIBRARY_PATH  # for PVR binaries
+
 # source the janus script
-source "$DIR"/janus/scripts/janus-conda-linux.sh
 # add pix2face to pythonpath
+export JANUS_SOURCE="$DIR"/janus
+export PYTHONPATH=${JANUS_SOURCE}/python:${PYTHONPATH}  # prepend with janus source
 export PYTHONPATH="$DIR"/pix2face:$PYTHONPATH
 
