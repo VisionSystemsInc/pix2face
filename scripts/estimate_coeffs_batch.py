ed0e60c6a69f9680f8e94f9ad28342ae424986e8
Daniel Crispell <dan@visionsystemsinc.com>
2018-02-28 21:57:22 -0500

    add parallel processing

--- scripts/estimate_coeffs_batch.py
+++ scripts/estimate_coeffs_batch.py
@@ -6,34 +6,52 @@ import numpy as np
 from PIL import Image
 import pix2face.test
 import pix2face_estimation.coefficient_estimation
+from torch.multiprocessing import Pool
 
 cuda_device = 0
 num_subject_coeffs=30
 num_expression_coeffs=20
 
+num_threads = 1
+
 this_dir = os.path.dirname(__file__)
 pvr_data_dir = os.path.join(this_dir,'../janus/components/pvr/data_3DMM')
 
+# load the 3DMM data
+mm_data = pix2face_estimation.coefficient_estimation.load_pix2face_data(pvr_data_dir, num_subject_coeffs, num_expression_coeffs)
+# load the pix2face network
+pix2face_net = pix2face.test.load_pretrained_model(cuda_device=cuda_device)
+
+def process_image(img_fname):
+    print(img_fname)
+    img = np.array(Image.open(img_fname))
+    try:
+        coeffs = pix2face_estimation.coefficient_estimation.estimate_coefficients(img, pix2face_net, mm_data, cuda_device=cuda_device)
+    except pix2face_estimation.coefficient_estimation.CoefficientEstimationError:
+        print('Failed to estimate coefficients for ' + img_fname)
+        return
+    basename = os.path.splitext(os.path.basename(img_fname))[0]
+    output_fname = os.path.join(output_dir, basename + '_coeffs.txt')
+    coeffs.save(output_fname)
+    return
+
 def main(input_fname, output_dir):
-    # load the 3DMM data
-    mm_data = pix2face_estimation.coefficient_estimation.load_pix2face_data(pvr_data_dir, num_subject_coeffs, num_expression_coeffs)
-    # load the pix2face network
-    pix2face_net = pix2face.test.load_pretrained_model(cuda_device=cuda_device)
-    # open input and output files, process one file per line
+    # open input file, read one filename per line
+    img_fnames = []
     with open(input_fname, 'r') as ifd:
         for line in ifd:
             img_fname = line.strip()
-            print(img_fname)
-            img = np.array(Image.open(img_fname))
-            try:
-                coeffs = pix2face_estimation.coefficient_estimation.estimate_coefficients(img, pix2face_net, mm_data, cuda_device=cuda_device)
-            except pix2face_estimation.coefficient_estimation.CoefficientEstimationError:
-                print('Failed to estimate coefficients for ' + img_fname)
-                continue
+            img_fnames.append(img_fname)
+    print('Read %d image filenames' % len(img_fnames))
 
-            basename = os.path.splitext(os.path.basename(img_fname))[0]
-            output_fname = os.path.join(output_dir, basename + '_coeffs.txt')
-            coeffs.save(output_fname)
+    if num_threads > 1:
+        pool = Pool(num_threads)
+        _ = pool.map(process_image, img_fnames)
+    else:
+        for fname in img_fnames:
+            process_image(fname)
+
+    print('done.')
 
 if __name__ == '__main__':
     if len(sys.argv) != 3:
