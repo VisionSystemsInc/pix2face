7c1d58e96dab98069e9d187178025f224b8712f1
Daniel Crispell <dan@visionsystemsinc.com>
2018-03-01 10:16:43 -0500

    process files in chunks for increased efficiency

--- scripts/estimate_coeffs_batch.py
+++ scripts/estimate_coeffs_batch.py
@@ -12,27 +12,32 @@ cuda_device = 0
 num_subject_coeffs=30
 num_expression_coeffs=20
 
-num_threads = 1
+num_threads = 4
 
 this_dir = os.path.dirname(__file__)
 pvr_data_dir = os.path.join(this_dir,'../janus/components/pvr/data_3DMM')
 
 # load the 3DMM data
 mm_data = pix2face_estimation.coefficient_estimation.load_pix2face_data(pvr_data_dir, num_subject_coeffs, num_expression_coeffs)
-# load the pix2face network
-pix2face_net = pix2face.test.load_pretrained_model(cuda_device=cuda_device)
 
-def process_image(img_fname):
-    print(img_fname)
-    img = np.array(Image.open(img_fname))
-    try:
-        coeffs = pix2face_estimation.coefficient_estimation.estimate_coefficients(img, pix2face_net, mm_data, cuda_device=cuda_device)
-    except pix2face_estimation.coefficient_estimation.CoefficientEstimationError:
-        print('Failed to estimate coefficients for ' + img_fname)
-        return
-    basename = os.path.splitext(os.path.basename(img_fname))[0]
-    output_fname = os.path.join(output_dir, basename + '_coeffs.txt')
-    coeffs.save(output_fname)
+def process_chunk(fnames):
+    # load the pix2face network
+    pix2face_net = pix2face.test.load_pretrained_model(cuda_device=cuda_device)
+    images = []
+    for img_fname in fnames:
+        img = np.array(Image.open(img_fname))
+        images.append(img)
+
+    coeffs_list = pix2face_estimation.coefficient_estimation.estimate_coefficients_batch(images, pix2face_net, mm_data, cuda_device=cuda_device)
+    assert len(coeffs_list) == len(fnames)
+    for coeffs, img_fname in zip(coeffs_list, fnames):
+        print(img_fname)
+        if coeffs is None:
+            print('Failed to estimate coefficients for ' + img_fname)
+            continue
+        basename = os.path.splitext(os.path.basename(img_fname))[0]
+        output_fname = os.path.join(output_dir, basename + '_coeffs.txt')
+        coeffs.save(output_fname)
     return
 
 def main(input_fname, output_dir):
@@ -44,13 +49,14 @@ def main(input_fname, output_dir):
             img_fnames.append(img_fname)
     print('Read %d image filenames' % len(img_fnames))
 
+    chunk_size = 64
+    chunks = [img_fnames[i:i+chunk_size] for i in range(0,len(img_fnames),chunk_size)]
     if num_threads > 1:
         pool = Pool(num_threads)
-        _ = pool.map(process_image, img_fnames)
+        _ = pool.map(process_chunk, chunks)
     else:
-        for fname in img_fnames:
-            process_image(fname)
-
+        for chunk in chunks:
+            process_chunk(chunk)
     print('done.')
 
 if __name__ == '__main__':
